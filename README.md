c90563f36a5b32fb8717a43403a5fe93afc01a67
jianing <brucewang0811@gmail.com>
2021-03-01 21:14:32 -0800

    also print the last sample in the 1st group

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 CSE 280A Project - extrachromosomal DNA (ecDNA) in cancer
 
-*Strategy **1*** (`data/oncoprint_1.tsv`): ecDNA- = Non-circular, ecDNA+ = otherwise
+*Strategy **1*** (`data/oncoprint_1.tsv`):
 
-*Strategy **2*** (`data/oncoprint_2.tsv`): ecDNA+ = Circular, ecDNA- = otherwise
+* ecDNA- = Non-circular, ecDNA+ = otherwise
+* last ecDNA+ patient is *TCGA-VD-AA8P* (Col BNT in Excel)
+
+*Strategy **2*** (`data/oncoprint_2.tsv`):
+
+* ecDNA+ = Circular, ecDNA- = otherwise
+* last ecDNA+ patient is *TCGA-G2-A2EK* (Col BJF in Excel)
--- src/analysis.py
+++ src/analysis.py
@@ -16,13 +16,17 @@ res = res[['track_name'] + list(res.columns[res.columns.isin(ref['patient_barcod
 # Strategy 1: ecDNA- = Non-circular; ecDNA+ = otherwise
 ecdna_neg = ref[ref['sample_classification'] == 'Non-circular']['patient_barcode']
 ecdna_pos = ref[ref['sample_classification'] != 'Non-circular']['patient_barcode']
-cols_met1 = ['track_name'] + list(res.columns[res.columns.isin(ecdna_pos)]) + list(res.columns[res.columns.isin(ecdna_neg)])
+pos1 = list(res.columns[res.columns.isin(ecdna_pos)])
+print("Oncoprint1: {}, {}".format(pos1[-1], len(pos1)))
+cols_met1 = ['track_name'] + pos1 + list(res.columns[res.columns.isin(ecdna_neg)])
 df1 = res[cols_met1]
 df1.to_csv("../data/oncoprint_1.tsv", sep="\t", index=False)
 
 # Strategy 2: ecDNA+ = Circular; ecDNA- = otherwise
 ecdna_pos = ref[ref['sample_classification'] == 'Circular']['patient_barcode']
 ecdna_neg = ref[ref['sample_classification'] != 'Circular']['patient_barcode']
-cols_met2 = ['track_name'] + list(res.columns[res.columns.isin(ecdna_pos)]) + list(res.columns[res.columns.isin(ecdna_neg)])
+pos2 = list(res.columns[res.columns.isin(ecdna_pos)])
+print("Oncoprint2: {}, {}".format(pos2[-1], len(pos2)))
+cols_met2 = ['track_name'] + pos2 + list(res.columns[res.columns.isin(ecdna_neg)])
 df2 = res[cols_met1]
 df2.to_csv("../data/oncoprint_2.tsv", sep="\t", index=False)
